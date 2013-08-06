e16b1c53e17a11fdb65698e46486599bb89258ec
Nikita B. Zuev <nikitazu@gmail.com>
2013-08-06 23:19:34 +0400

    Moved FontAwesome icon constants to NyaWatch.Core
    win: Use font awesome constants from NyaWatch.Core
    osx: TODO - same thing

---
+++ NyaWatch.Core/Fonts/Awesome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NyaWatch.Core.Fonts
+{
+    /// <summary>
+    /// Unicode values of FontAwesome icons.
+    /// </summary>
+    public static class Awesome
+    {
+        public const string FastForwardIcon = "\uf050";
+        public const string PlayIcon = "\uf04b";
+        public const string StopIcon = "\uf04d";
+        public const string PauseIcon = "\uf04c";
+        public const string EjectIcon = "\uf052";
+    }
+}
