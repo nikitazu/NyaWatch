da3aab85a2873aba9f1e8e20809a7de9194fe3bf
Nikita B. Zuev <nikitazu@gmail.com>
2013-08-11 11:46:52 +0400

    win: implemented application menu

---
+++ NyaWatch.Windows/WPF/Converters/ConverterBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Globalization;
+using System.Windows.Markup;
+using System.Windows.Data;
+
+namespace NyaWatch.Windows.WPF.Converters
+{
+    public abstract class ConverterBase<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        /// <summary>
+        /// Must be implemented in inheritor.
+        /// </summary>
+        public abstract object Convert(object value, Type targetType, object parameter,
+            CultureInfo culture);
+
+        /// <summary>
+        /// Override if needed.
+        /// </summary>
+        public virtual object ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region MarkupExtension members
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (_converter == null)
+                _converter = new T();
+            return _converter;
+        }
+
+        private static T _converter = null;
+
+        #endregion
+    }
+}
---
+++ NyaWatch.Windows/WPF/Converters/NullToBool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace NyaWatch.Windows.WPF.Converters
+{
+    public class NullToBool : ConverterBase<NullToBool>
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value != null;
+        }
+    }
+}
